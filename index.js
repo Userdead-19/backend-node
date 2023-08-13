c0871afb18acdf5668f145bb8fa912b21f691b2d
Userded-19 <abinav10102@gmail.com>
2023-08-13 22:27:10 +0530

    login route

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ app.listen(port, () => console.log(`Server is running on port ${port}`));
 const User = require("./models/UserModel");
 const Message = require("./models/MessageModel");
 
+const createToken = (userid) => {
+  const payload = { userid: userid };
+
+  const token = jwt.sign(payload, "#0sdfj58%s5", { expiresIn: "1h" });
+
+  return token;
+};
+
 app.post("/register", (req, res) => {
   const { name, email, password, image } = req.body;
 
@@ -39,6 +47,29 @@ app.post("/register", (req, res) => {
     .then((user) => res.status(200).json({ message: user }))
     .catch((err) => {
       console.log("error in register");
+      res.status(400).json({ message: err });
+    });
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        res.status(400).json({ message: "User not found" });
+      }
+
+      if (user.password !== password) {
+        res.status(400).json({ message: "Password incorrect" });
+      }
+
+      const token = createToken(user._id);
+      res.status(200).json(token);
+    })
+    .catch((err) => {
+      console.log("error in finding the user", err);
+      ("");
       res.status(400).json({ message: err });
     });
 });
