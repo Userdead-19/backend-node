86ce6b9056da44bca790ec3c5e9f12f4cf491e8e
Abinav <121537988+Userdead-19@users.noreply.github.com>
2023-08-15 12:33:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,18 +31,6 @@ app.listen(port, () => console.log(`Server is running on port ${port}`));
 const User = require("./models/UserModel");
 const Message = require("./models/MessageModel");
 
-const createToken = (userId) => {
-  const payload = { userId: userId };
-
-  const token = jwt.sign(
-    payload,
-    "Q$r2K6W8n!jCW%Zk",
-    { expiresIn: "1h" }
-  );
-
-  return token;
-};
-
 app.post("/register", (req, res) => {
   const { name, email, password, image } = req.body;
 
@@ -56,42 +44,61 @@ app.post("/register", (req, res) => {
     });
 });
 
+const createToken = (userId) => {
+  // Set the token payload
+  const payload = {
+    userId: userId,
+  };
+
+  // Generate the token with a secret key and expiration time
+  const token = jwt.sign(payload, "Q$r2K6W8n!jCW%Zk", { expiresIn: "1h" });
+
+  return token;
+};
+
+//endpoint for logging in of that particular user
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
+  //check if the email and password are provided
+  if (!email || !password) {
+    return res
+      .status(404)
+      .json({ message: "Email and the password are required" });
+  }
+
+  //check for that user in the database
   User.findOne({ email })
     .then((user) => {
       if (!user) {
-        res.status(400).json({ message: "User not found" });
+        //user not found
+        return res.status(404).json({ message: "User not found" });
       }
 
+      //compare the provided passwords with the password in the database
       if (user.password !== password) {
-        res.status(400).json({ message: "Password incorrect" });
+        return res.status(404).json({ message: "Invalid Password!" });
       }
 
       const token = createToken(user._id);
       res.status(200).json({ token });
     })
-    .catch((err) => {
-      console.log("error in finding the user", err);
-      ("");
-      res.status(400).json({ message: err });
+    .catch((error) => {
+      console.log("error in finding the user", error);
+      res.status(500).json({ message: "Internal server Error!" });
     });
 });
 
-app.get("/", (req, res) => {
-  console.log("hello");
-  res.send("Hello World!");
-});
+//endpoint to access all the users except the user who's is currently logged in!
+app.get("/users/:userId", (req, res) => {
+  const loggedInUserId = req.params.userId;
 
-app.get("/user/:userid", (req, res) => {
-  const userid = req.params.userid;
-  User.find({ _id: { $ne: userid } })
+  User.find({ _id: { $ne: loggedInUserId } })
     .then((users) => {
       res.status(200).json(users);
     })
     .catch((err) => {
-      console.log("eror in finding users", err);
-      res.status(400).json({ message: err });
+      console.log("Error retrieving users", err);
+      res.status(500).json({ message: "Error retrieving users" });
     });
 });
