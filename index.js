8897d268120cf374e2edaba9959cf0e57e66364d
Userded-19 <abinav10102@gmail.com>
2023-08-15 11:58:07 +0530

    addded morgan

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-
+const morgan = require("morgan");
 const app = express();
 const port = 5000;
 const cors = require("cors");
 app.use(cors());
 
+app.use(morgan("tiny"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
