238fe4b68d5506dd9752ad44c870109f38302354
Userded-19 <abinav10102@gmail.com>
2023-08-20 15:54:57 +0530

    changed the scheme in usermodel

--- models/UserModel.js
+++ models/UserModel.js
@@ -18,6 +18,46 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  gender: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: String,
+    required: true,
+  },
+  yearofgraduate: {
+    type: String,
+    required: true,
+  },
+  branch: {
+    type: String,
+    required: true,
+  },
+  college: {
+    type: String,
+    required: true,
+  },
+  fieldofinterest: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: String,
+    required: true,
+  },
+  placeofwork: {
+    type: String,
+    required: true,
+  },
+  designation: {
+    type: String,
+    required: true,
+  },
+  about: {
+    type: String,
+    required: true,
+  },
   friendRequests: [
     {
       type: mongoose.Schema.Types.ObjectId,
